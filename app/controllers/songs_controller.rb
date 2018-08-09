344cdac45d701647c46c28a5d3a3167e12736982
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-09 15:06:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,11 +29,11 @@ class SongsController < ApplicationController
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
-    binding.pry
     erb :'/songs/edit'
   end
 
   post '/songs/:slug/edit' do
+    binding.pry
     artist = Artist.find_or_create_by(name: params["song"]["artist"]["name"])
     song = Song.find_by_slug(params[:slug])
 
